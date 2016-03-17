6ffdbf6929955a2bc89118561b6e0e046cd064fe
James Young <jamthoyoung@gmail.com>
2016-03-17 15:25:52 -0400

    propTypes chapter

--- views/index.jsx
+++ views/index.jsx
@@ -19,6 +19,7 @@ class TodoList extends React.Component {
                         <tbody>
                         <Todo title="Shopping">Milk</Todo>
                         <Todo title="Hair cut">13:00</Todo>
+                        <Todo title="hah">15:00</Todo>
                         </tbody>
                     </table>
            </div>
@@ -36,6 +37,10 @@ class Todo extends React.Component {
      );
   }
 }
+Todo.propTypes = {
+  title: React.PropTypes.string.isRequired
+};
+
 
 class TodoForm extends React.Component{
   render() {
