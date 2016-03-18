7be90849e27c4c951f55c229e384544c8d8b65b9
James Young <jamthoyoung@gmail.com>
2016-03-18 14:36:45 -0400

    server props

--- program.js
+++ program.js
@@ -9,9 +9,16 @@ app.engine('jsx', require('express-react-views').createEngine({ transformViews: 
 require('babel/register')({
   ignore: false
 });
+
+var data = [
+   { 'title' : 'Shopping',
+     'detail' : process.argv[3] }, 
+   { 'title' : 'Hair cut',
+     'detail' : process.argv[4] }
+];
     
 app.use('/', function(req, res) {
-  res.render('index', '');
+  res.render('index', {data : data});
 });
     
 app.listen(app.get('port'), function() {});
